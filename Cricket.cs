ab919af7eb8ade19ea3a1075dc7b0502947eb3b3
kakoyetoimya <153298426+kakoyetoimya@users.noreply.github.com>
2023-12-08 20:14:01 +0500

    Add files via upload

---
+++ Cricket.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cricket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Ecco uno script, che verrà ripetuto, se non si vuole scrivere questo script, scriverlo all'inizio di //.
+    void Update()
+    {
+        print("Crickets");
+    }
+}
