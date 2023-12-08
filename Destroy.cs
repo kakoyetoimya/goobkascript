41f4a84df11ede3f8e9980bf7a6bbd1af5cae54c
kakoyetoimya <153298426+kakoyetoimya@users.noreply.github.com>
2023-12-08 20:27:01 +0500

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // "Destroy everything!" - фраза из рекламы роблокса, там где было "Roblox - is FREE!"
+    void Start()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
